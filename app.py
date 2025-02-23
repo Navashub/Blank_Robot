3acf9d4a9b0bc80d6d8ac895f845b7791700c19a
Navashub <navasmuller01@gmail.com>
2025-02-23 21:51:10 +0300

    hello

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ from routes.robot_routes import robot_bp
 app = Flask(__name__)
 app.register_blueprint(robot_bp)
 
+@app.route('/')
+def home():
+    return "Hello World!"
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
